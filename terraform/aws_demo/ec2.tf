ca8979d0328b6708081f3a1890c1839e91faf718
Micah Martin <micahlmartin@gmail.com>
2020-11-10 13:59:48 -0500

    Updated

--- terraform/aws_demo/ec2.tf
+++ terraform/aws_demo/ec2.tf
@@ -1,22 +1,6 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 resource "aws_instance" "web" {
   count         = 3
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-0947d2ba12ee1ff75"
   instance_type = "t3.micro"
   associate_public_ip_address = true
   subnet_id = "subnet-06ae0df47f1cd3b23"
