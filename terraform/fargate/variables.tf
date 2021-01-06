018aa4c486ace56d28e7d2420d48717d84c800df
Micah Martin <micahlmartin@gmail.com>
2021-01-06 10:08:03 -0500

    Updated

--- terraform/fargate/variables.tf
+++ terraform/fargate/variables.tf
@@ -3,13 +3,13 @@ variable "name" {
 }
 
 variable "vpc_id" {
-  value = "vpc-0e7bedb2e71dd8025"
+  default = "vpc-0e7bedb2e71dd8025"
 }
 
 variable "subnet_id" {
-  value = "subnet-0d3650898e5d4ca9a"
+  default = "subnet-0d3650898e5d4ca9a"
 }
 
 variable "security_group_id" {
-  value = "sg-011b1f285fb3c9be1"
+  default = "sg-011b1f285fb3c9be1"
 }
