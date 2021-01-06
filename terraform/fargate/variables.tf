9a9fb92ccf5147523658d58e6eeb27698f7473b2
Micah Martin <micahlmartin@gmail.com>
2021-01-06 10:07:12 -0500

    Updated

--- terraform/fargate/variables.tf
+++ terraform/fargate/variables.tf
@@ -1,3 +1,15 @@
 variable "name" {
   default = "micah-fargate-test"
 }
+
+variable "vpc_id" {
+  value = "vpc-0e7bedb2e71dd8025"
+}
+
+variable "subnet_id" {
+  value = "subnet-0d3650898e5d4ca9a"
+}
+
+variable "security_group_id" {
+  value = "sg-011b1f285fb3c9be1"
+}
